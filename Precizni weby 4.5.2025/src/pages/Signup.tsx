e63d4b64c58be38bc50d4de999e165cd72b7ae38
stepanfisara <stepanfisara@gmail.com>
2025-05-04 12:23:22 +0200

    Delete Precizni weby 4.5.2025 directory

--- Precizni weby 4.5.2025/src/pages/Signup.tsx
+++
@@ -1,9 +0,0 @@
-import AuthForm from '../components/AuthForm';
-
-export default function Signup() {
-  return (
-    <div className="min-h-screen bg-black flex items-center justify-center p-4">
-      <AuthForm type="signup" />
-    </div>
-  );
-}
